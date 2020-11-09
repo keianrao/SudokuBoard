505ebbbc5b5c7f38a9dc635d8eb8570c090755d6
Keian Rao <keian.rao@gmail.com>
2020-11-09 15:40:06 -0500

    Added major lines. Changed painting strategy

--- SudokuBoard.java
+++ SudokuBoard.java
@@ -11,11 +11,11 @@ class SudokuBoard extends JPanel {
 	
 	Also, I'm going to write in canonical Sun style this time.
 	*/
-	
-	private static final int COLUMNS = 9;
-	private static final int ROWS = 9;
-	int[][] board = new int[COLUMNS][ROWS];
 
+	int[][] board = new int[9][9];
+	// We won't design for changing the number of columns and rows, because
+	// it's programatically difficult to support multiple configurations.
+	// And Congkopi certainly won't have varying configurations.
 
 	SudokuBoard() {
 		
@@ -35,44 +35,55 @@ class SudokuBoard extends JPanel {
 	protected void paintComponent(Graphics g) {
 		g.clearRect(0, 0, getWidth(), getHeight());
 		
-		int verticalLines = COLUMNS + 1;
-		int horizontalLines = ROWS + 1;
-		int verticalLineWidth = 2;
-		int horizontalLineWidth = 2;
+		// For simplicity's sake, and also to match Congkopi, let's
+		// have constant tile sizes. This means we won't scale the tiles 
+		// up to fit the window.. which also means we don't have to
+		// scale mouse coordinates to logical coordinates.
+		final int tileWidth = 40 + 1;
+		final int tileHeight = tileWidth + 1;		
+		// Okay, here's our strategy regarding the grid lines.
+		// Game-logic-wise, we'll actually have no boundaries between lines.
+		// So we draw the tiles first as a simple grid, and then we paint
+		// grid lines on top of where they meet. Only for rendering.
+		// Therefore, we increase the tile size a bit so that the
+		// exposed area after painting the grid lines will come back to.
 		
-		// For simplicity's sake, let's have constant tile sizes.
-		// This means we won't scale the tiles up to fit the window.
-		final int tileWidth = 40;
-		final int tileHeight = tileWidth;
-		
-		// Given the tile widths, number of columns, and vertical line widths,
-		// we can determine board width. Similarly for height.
-		int boardWidth =
-			(verticalLines * 2)
-			+ (COLUMNS * tileWidth);
-		int boardHeight =
-			(horizontalLines * 2)
-			+ (ROWS * tileHeight);
-			
-		// Okay. Fill the board area..
+		// So. Fill the board area first.
+		int boardWidth = tileWidth * 9;
+		int boardHeight = tileHeight * 9;
 		int startX = (getWidth() - boardWidth) / 2;
 		int startY = (getHeight() - boardHeight) / 2;
 		g.setColor(getBackground().darker());
 		g.fillRect(startX, startY, boardWidth, boardHeight);
 		
+		// Later on, we should iterate across the board.
+		// Selected tile, paint a different colour.
+		// Otherwise, paint the filled number if present.
+		
 		g.setColor(getForeground());
-		// ..then draw the lines.
-		// There needs to be tileWidth/tileHeight between every line.
-		// We have x as always the topleft of the current line -
-		// so we advance rightwards first by line width, then by tilewidth.
-		int xAdv = verticalLineWidth + tileWidth;
-		for (int x = 0; x <= boardWidth; x += xAdv) {
-			g.fillRect(startX + x, startY, verticalLineWidth, boardHeight);
+		// Now we'll iterate over the equally-spaced tile boundaries.
+		for (int column = 1; column <= 9 + 1; ++column) {
+			int left = (column - 1) * tileWidth;
+			
+			boolean leftIsMajorLine = ( (column - 1) % 3 ) == 0;
+			int lineWidth = leftIsMajorLine ? 4 : 2;
+			
+			// Okay, so. The line we're about to paint,
+			// we'll horizontally center it on the boundary.
+			int lineLeft = left - (lineWidth / 2);
+			g.fillRect(startX + lineLeft, startY, lineWidth, boardHeight);
 		}
-		int yAdv = horizontalLineWidth + tileHeight;
-		for (int y = 0; y <= boardHeight; y += yAdv) {
-			g.fillRect(startX, startY + y, boardWidth, horizontalLineWidth);
+		
+		// Now do the same for rows.
+		for (int row = 1; row <= 9 + 1; ++row) {
+			int top = (row - 1) * tileHeight;
+			boolean topIsMajorLine = ( (row - 1) % 3 ) == 0;
+			int lineWidth = topIsMajorLine ? 4 : 2;			
+			int lineTop = top - (lineWidth / 2);
+			g.fillRect(startX, startY + lineTop, boardWidth, lineWidth);
 		}
+		// (We can actually combine the two loops, but let's
+		// keep them separate for readability)
 	}
 
 }
